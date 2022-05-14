cbb5c119784ff4583d31ee7e2b3b867bef7881cc
Clay Dowling <clay@lazarusid.com>
2022-05-14 09:16:55 -0400

    is_* functions work

--- test/test_ast.cpp
+++ test/test_ast.cpp
@@ -4,6 +4,17 @@ extern "C" {
   #include "ast.h"
 }
 
+TEST(Ast, is_skill_returns_true_when_given_skill) {
+  struct skill_t* s = new_skill("Target", attr_agility);
+  EXPECT_TRUE(is_skill((void*)s, "Target"));
+}
+
+TEST(Ast, is_skill_returns_false_when_given_not_skill) {
+  struct skill_t* s = new_skill("Not It", attr_agility);
+  EXPECT_FALSE(is_skill((void*)s, "Something Else"));
+}
+
+
 TEST(Ast, new_world_creates_null_initialized_word) {
     struct world_t* world = new_world();
 
