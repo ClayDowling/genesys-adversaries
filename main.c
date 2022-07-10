cd6ea688467f40f1acbf7d6b66de6b645ed98265
Clay Dowling <clay@lazarusid.com>
2022-07-10 13:03:18 -0400

    Add data dir to search path

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "parser.h"
 #include "character.h"
+#include "lexer.h"
 
 void about(void) {
     puts("Genesys RPG Adversary Builder\n"
@@ -12,6 +15,19 @@ void about(void) {
     );
 }
 
+const char* basename(const char* filename) {
+    static char path[PATH_MAX];
+    char *end;
+#ifdef _WIN64
+    end = strrchr(filename, '\\');
+#else
+    end = strrchr(filename, '/');
+#endif
+    if (NULL == end) return ".";
+    strncpy(path, filename, end - filename);
+    return path;
+}
+
 int main(int argc, const char* argv[]) {
 
     if (argc == 1) {
@@ -19,6 +35,14 @@ int main(int argc, const char* argv[]) {
         return EXIT_FAILURE;
     }
 
+    char librarypath[PATH_MAX];
+#ifdef _WIN64
+    snprintf(librarypath, PATH_MAX, "%s\\data", basename(argv[0]));
+#else
+    snprintf(librarypath, PATH_MAX, "%s/data", basename(argv[0]));
+#endif
+    lex_add_directory(librarypath);
+    
     struct world_t *world = parse_file(argv[1]);
     struct namedlist_t *character;
     for(struct node_t *cur = world->characters; cur != NULL; cur = cur->next) {
